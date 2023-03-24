83741927a009b8e96c5f2321a63ce29665b7de29
Daniel <hdanielmm@users.noreply.github.com>
2023-03-23 23:04:12 -0500

    feat: add remote db connection

--- backend/FastApi/db/client.py
+++ backend/FastApi/db/client.py
@@ -1,3 +1,17 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-db_client = MongoClient()
+load_dotenv()
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+
+# Base de datos local
+
+# db_client = MongoClient().local
+
+# Base de datos remota
+
+client = MongoClient("mongodb+srv://" + user + ":" + password + "@cluster0.ypgfhts.mongodb.net/?retryWrites=true&w=majority")
+db_client = client.test
--- backend/FastApi/routers/users_db.py
+++ backend/FastApi/routers/users_db.py
@@ -17,7 +17,7 @@ user_list = []
 
 @router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(db_client.users.find())
 
 
 # Path
@@ -41,9 +41,9 @@ async def add_user(user: User):
     user_dict = dict(user)
     del user_dict["id"]
 
-    id = db_client.local.users.insert_one(user_dict).inserted_id
+    id = db_client.users.insert_one(user_dict).inserted_id
 
-    new_user = user_schema(db_client.local.users.find_one({"_id": id}))
+    new_user = user_schema(db_client.users.find_one({"_id": id}))
 
     return User(**new_user)
 
@@ -54,7 +54,7 @@ def put_user(user: User):
     del user_dict["id"]
 
     try:
-        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+        db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist")
 
@@ -63,7 +63,7 @@ def put_user(user: User):
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(id: str):
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found:
         return {"Error": "No user deleted"}
@@ -71,7 +71,7 @@ async def delete_user(id: str):
 
 def search_user(field: str, key):
     try:
-        user = db_client.local.users.find_one({field: key})
+        user = db_client.users.find_one({field: key})
         return User(**user_schema(user))
     except:
         return {"Error": "User not found"}
